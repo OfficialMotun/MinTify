b00c56de15a1d4938acc249adbcf58c23252f9fc
Your Name <your.email@example.com>
2024-10-07 00:31:04 +0100

    added features

---
+++ frontend/src/pages/Components/footer.js
@@ -0,0 +1,23 @@
+export default function Footer() {
+    return (
+      <div className="bg-[#353747] py-10">
+        
+        <div className="px-[50px] sm:px-[100px] sm:flex justify-between">
+          
+          
+          <div className="sm:flex sm:space-x-6 space-y-6 sm:space-y-0 font-semibold sm:text-[16px]">
+            <h1>Contact Us</h1>
+            <h1>Privacy Policy</h1>
+            <h1>FAQs</h1>
+            <h1>About us</h1>
+          </div>
+
+          <div className="pt-[40px] sm:pt-[0px]">
+            Logo
+          </div>
+  
+        </div>
+      </div>
+    );
+  }
+  
