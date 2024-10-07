7ec1e23db7fa252233445d6f8591e0d4825f31f3
JustFavour <odeyemifavour1@gmail.com>
2024-10-07 15:14:35 +0100

    added deployment script

--- Web3Backend/hardhat.config.js
+++ Web3Backend/hardhat.config.js
@@ -1,6 +1,37 @@
 require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+const dotenv = require("dotenv");
+dotenv.config();
 module.exports = {
   solidity: "0.8.27",
+  optimizer: {
+    enabled: true,
+    run: 200,
+  },
+
+  networks: {
+    // for testnet
+    "base-sepolia": {
+      url: "https://sepolia.base.org",
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      "base-sepolia": process.env.API_KEY,
+    },
+    customChains: [
+      {
+        network: "base-sepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://api-sepolia.basescan.org/api",
+          browserURL: "https://sepolia.basescan.org",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: false,
+  },
 };
