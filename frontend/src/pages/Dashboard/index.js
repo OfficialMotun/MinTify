45fc4cbd263b96cc9f5d9a88a201c5c76408e976
Your Name <your.email@example.com>
2024-10-11 19:38:21 +0100

    added animation

---
+++ frontend/src/pages/Dashboard/index.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Dashboard() {
+  
+
+  return (
+    <div className="grid space-y-6  justify-center my-[200px]">
+
+<Image src="/logo.png" alt="logo" width={100} height={100}  className="pl-[20px]"/>
+
+
+        <div className="flex">
+           
+            <h1 className="text-[20px] sm:text-[40px]">Coming soon..</h1>
+        </div>
+       
+      
+    </div>
+  );
+}
